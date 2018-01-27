4f50bdb87815c144b775766d64618099d1610fcc
advaypakhale <advay.pakhale@gmail.com>
2018-01-27 23:40:32 +0800

    nth

--- models/research/object_detection/tfimgcontroller.py
+++ models/research/object_detection/tfimgcontroller.py
@@ -13,40 +13,36 @@ from PIL import Image
 
 import cv2
 
-from models.research.object_detection.utils import label_map_util
-from models.research.object_detection.utils import visualization_utils as vis_util
-
-
 class FaceRecognition:
-    MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
-    MODEL_FILE = MODEL_NAME + '.tar.gz'
-    DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
-    PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
-    PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
-    NUM_CLASSES = 90
-
     def __init__(self):
         self.cap = cv2.VideoCapture(0)
-        sys.path.append("..")
-        sys.path.append("data")
-
+        sys.path.append("/Users/advaypakhale/Documents/Projects/HacknRoll18/models/research")
+        sys.path.append("/Users/advaypakhale/Documents/Projects/HacknRoll18/models/research/object_detection/data")
+        from utils import label_map_util
+        from utils import visualization_utils as vis_util
+        MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
+        MODEL_FILE = MODEL_NAME + '.tar.gz'
+        DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+        PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+        PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+        NUM_CLASSES = 90
         self.detection_graph = tf.Graph()
         with self.detection_graph.as_default():
             od_graph_def = tf.GraphDef()
-            with tf.gfile.GFile(self.PATH_TO_CKPT, 'rb') as fid:
+            with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
                 serialized_graph = fid.read()
                 od_graph_def.ParseFromString(serialized_graph)
                 tf.import_graph_def(od_graph_def, name='')
-        label_map = label_map_util.load_labelmap(self.PATH_TO_LABELS)
-        categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=self.NUM_CLASSES,
+        label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+        categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
                                                                     use_display_name=True)
-        self.category_index = label_map_util.create_category_index(categories)
+        category_index = label_map_util.create_category_index(categories)
         self.x = 0
 
     def capture(self, dt):
         with self.detection_graph.as_default():
             with tf.Session(graph=self.detection_graph) as sess:
-                ret, image_np = self.cap.read()
+                ret, image_np = cap.read()
                 image_np_expanded = np.expand_dims(image_np, axis=0)
                 image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
                 boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
@@ -65,11 +61,12 @@ class FaceRecognition:
                     np.squeeze(boxes),
                     np.squeeze(classes).astype(np.int32),
                     np.squeeze(np.array(fscores)),
-                    self.category_index,
+                    category_index,
                     use_normalized_coordinates=True,
                     line_thickness=8)
                 cv2.imshow('object detection', cv2.resize(image_np, (800, 600)))
                 if cv2.waitKey(25) & 0xFF == ord('q'):
                     cv2.destroyAllWindows()
+                    break
 
                 self.x = (boxes[0][person_elements[0]][1] + boxes[0][person_elements[0]][3])/2
